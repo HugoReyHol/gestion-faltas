189c1188c764e79a73328095219bbfa9b6724317
Hugo <hugo.reyhol@educa.jcyl.es>
2024-11-26 09:13:36 +0100

    fxml de login funcional y controller preparado para clases

--- src/main/java/org/dam2/gestionfaltas/controller/LoginCtrll.java
+++ src/main/java/org/dam2/gestionfaltas/controller/LoginCtrll.java
@@ -1,11 +1,26 @@
 package org.dam2.gestionfaltas.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import org.dam2.gestionfaltas.util.HibernateUtil;
 import org.hibernate.Session;
 
 
 public class LoginCtrll {
 
+    @FXML
+    public TextField inNumero;
+
+    @FXML
+    public PasswordField inContrasena;
+
     private final Session session = HibernateUtil.getSession();
 
+
+    public void onLogIn(ActionEvent actionEvent) {
+        // TODO implementar logica de verificacion de usuario cuando tengamos las clases y el dao
+
+    }
 }
