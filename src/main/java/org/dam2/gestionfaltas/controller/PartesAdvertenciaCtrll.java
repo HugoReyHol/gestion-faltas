53cd557deb3293592a542f38c94caa58543b3a0c
DBelen99 <daysib.anator@educa.jcyl.es>
2024-11-27 10:42:28 +0100

    Crear fxml de partes de Advertencia e implementamos los metodos en el controller

--- src/main/java/org/dam2/gestionfaltas/controller/PartesAdvertenciaCtrll.java
+++ src/main/java/org/dam2/gestionfaltas/controller/PartesAdvertenciaCtrll.java
@@ -4,24 +4,64 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import org.dam2.gestionfaltas.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
 public class PartesAdvertenciaCtrll {
 
     @FXML
-    private Button bt_verde;
+    private Button bt_crear;
 
     @FXML
-    private ComboBox<?> cb_tipos;
+    private Button bt_parteNaranja;
+
+    @FXML
+    private Button bt_parteRojo;
+
+    @FXML
+    private Button bt_parteVerde;
+
+    @FXML
+    private ComboBox<?> cb_hora;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Label lbTitulo;
 
     @FXML
     private TextField tf_nExpediente;
 
     @FXML
-    void onVerde(ActionEvent event) {
+    private TextField tf_nombreGrupo;
+
+    @FXML
+    private TextField tf_profesor;
+
+    @FXML
+    private TextArea tx_descripcion;
+
+    @FXML
+    void onCrear(ActionEvent event) {
 
     }
+
+    @FXML
+    void onParteNaranja(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onParteRojo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onParteVerde(ActionEvent event) {
+
+    }
+
 }
