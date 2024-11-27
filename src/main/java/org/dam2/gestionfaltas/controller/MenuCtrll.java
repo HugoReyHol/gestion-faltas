383a20fd01e37e602a75b5cbf08e57e00fdf58ce
DBelen99 <daysib.anator@educa.jcyl.es>
2024-11-27 10:42:54 +0100

    Se crear menu controller

---
+++ src/main/java/org/dam2/gestionfaltas/controller/MenuCtrll.java
@@ -0,0 +1,42 @@
+package org.dam2.gestionfaltas.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuCtrll {
+
+    @FXML
+    private Button bt_crearParte;
+
+    @FXML
+    private Button bt_crearProfesor;
+
+    @FXML
+    private Button bt_listaAlumnos;
+
+    @FXML
+    private Button bt_listaPartes;
+
+    @FXML
+    void onCrearParte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCrearProfesor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onListaAlumnos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onListaPartes(ActionEvent event) {
+
+    }
+
+}
+
