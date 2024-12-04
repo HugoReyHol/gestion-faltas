4385637670af5f7d7cd2f106d3f61fa2e4a676b4
Hugo <hugo.reyhol@educa.jcyl.es>
2024-12-04 10:13:54 +0100

    Creado PuntosPartesDAOImpl

---
+++ src/main/java/org/dam2/gestionfaltas/dao/PuntosPartesDAOImpl.java
@@ -0,0 +1,33 @@
+package org.dam2.gestionfaltas.dao;
+
+import org.dam2.gestionfaltas.dao.interfaces.PuntosPartesDAO;
+import org.dam2.gestionfaltas.model.PuntosPartes;
+
+import java.util.List;
+
+public class PuntosPartesDAOImpl implements PuntosPartesDAO {
+    @Override
+    public void eliminar(int idPuntos) {
+
+    }
+
+    @Override
+    public void modificar(PuntosPartes puntosPartes) {
+
+    }
+
+    @Override
+    public void crear(PuntosPartes puntosPartes) {
+
+    }
+
+    @Override
+    public List<PuntosPartes> listar() {
+        return null;
+    }
+
+    @Override
+    public PuntosPartes obtener(int idPuntos) {
+        return null;
+    }
+}
