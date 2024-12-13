e798733ca6e91ce7559b921608f63bce3340be6e
Hugo <hugo.reyhol@educa.jcyl.es>
2024-12-13 21:30:53 +0100

    Implementada comprobación de errores en LoginCtrll

--- src/main/java/org/dam2/gestionfaltas/controller/LoginCtrll.java
+++ src/main/java/org/dam2/gestionfaltas/controller/LoginCtrll.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.dam2.gestionfaltas.dao.ProfesorDAOImpl;
+import org.dam2.gestionfaltas.model.Profesor;
+import org.dam2.gestionfaltas.util.Encriptador;
 
 
 public class LoginCtrll {
@@ -14,9 +17,46 @@ public class LoginCtrll {
     @FXML
     public PasswordField inContrasena;
 
+    private final ProfesorDAOImpl profesorDAO = new ProfesorDAOImpl();
+
 
     public void onLogIn(ActionEvent actionEvent) {
-        // TODO implementar logica de verificacion de usuario cuando tengamos las clases y el dao
+        // Comprueba si los campos están vacios
+        if (inNumero.getText().isBlank()) {
+            // TODO Alerta de introducir número
+            System.out.println("Numero vacio");
+            return;
+        }
+        if (inContrasena.getText().isBlank()) {
+            // TODO Alerta de introducir contraseña
+            System.out.println("Contra vacia");
+            return;
+        }
+
+        // Comprueba si el campo contiene 4 letras o menos
+        if (inNumero.getText().length() > 4) {
+            // TODO Alerta de introducir numero formato correcto
+            System.out.println("Formato malo");
+            return;
+        }
+
+        // Compruba si el usuario existe y en caso de existir si a contraseña es correcta
+        Profesor profesor = profesorDAO.obtener(inNumero.getText());
+
+        if (profesor == null) {
+            // TODO Alerta profesor no existe
+            System.out.println("Profe inventado");
+            return;
+        }
+
+        if (!profesor.getContrasena().equals(Encriptador.encriptar(inContrasena.getText()))) {
+            // TODO Alerta contraseña incorrecta
+            System.out.println("Contraseña mala");
+            return;
+        }
+
+        // Cambia a escena menu y pasa el profesor iniciado
+        System.out.println("Ha funcionao");
 
     }
 }
