08abf39086b915f210f494492ed65d3023168b6f
estelaV9 <esteladeveg@gmail.com>
2024-12-14 11:45:00 +0100

    referenciar los botones del menu a sus respectivas vistas

--- src/main/java/org/dam2/gestionfaltas/controller/MenuCtrll.java
+++ src/main/java/org/dam2/gestionfaltas/controller/MenuCtrll.java
@@ -3,6 +3,8 @@ package org.dam2.gestionfaltas.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import org.dam2.gestionfaltas.util.CambiarVista;
+import org.dam2.gestionfaltas.util.R;
 
 public class MenuCtrll {
 
@@ -20,23 +22,23 @@ public class MenuCtrll {
 
     @FXML
     void onCrearParte(ActionEvent event) {
-
-    }
+        CambiarVista.cambiarVistaBtt("partesAdvertencia.fxml", bt_crearParte, "Crear Parte Advertencia");
+    } // IR A LA VISTA DE CREAR PARTE
 
     @FXML
     void onCrearProfesor(ActionEvent event) {
-
-    }
+        //CambiarVista.cambiarVistaBtt(".fxml", bt_crearParte, "Crear Profesor");
+    } // IR A LA VISTA DE CREAR PROFESORES
 
     @FXML
     void onListaAlumnos(ActionEvent event) {
-
-    }
+        CambiarVista.cambiarVistaBtt("listaAlumnos.fxml", bt_listaAlumnos, "Lista de Alumnos");
+    } // IR A LA VISTA DE LISTA DE ALUMNOS
 
     @FXML
     void onListaPartes(ActionEvent event) {
-
-    }
+        CambiarVista.cambiarVistaBtt("listaPartes.fxml", bt_listaPartes, "Lista de Partes");
+    } // IR A LA VISTA DE LISTA DE PARTES
 
 }
 
