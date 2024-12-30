10ddfd45bbd876cc395835bb5205353d1255fe05
Hugo <hugo.reyhol@educa.jcyl.es>
2024-12-30 23:21:04 +0100

    Añadido método para confirmar acciones en AlertUtil

--- src/main/java/org/dam2/gestionfaltas/util/AlertUtil.java
+++ src/main/java/org/dam2/gestionfaltas/util/AlertUtil.java
@@ -1,6 +1,7 @@
 package org.dam2.gestionfaltas.util;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 
 public class AlertUtil {
@@ -14,5 +15,18 @@ public class AlertUtil {
         Alert alerta = new Alert(Alert.AlertType.INFORMATION);
         alerta.setContentText(mensaje);
         alerta.showAndWait();
+    }
+
+    /*
+    * Devuelve true si acepta o false si cancela
+    * */
+    public static boolean crearConfirmacion(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Información");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+
+        return alert.getResult() != ButtonType.CANCEL;
     }
 }
